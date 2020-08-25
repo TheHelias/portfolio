97871345307eae7549df9b7f5953ce5b037aebdd
TheHelias <akande.tolu@yahoo.com>
2020-08-25 12:11:09 +0100

    initial setup

---
+++ src/mock/data.js
@@ -0,0 +1,101 @@
+import { nanoid } from 'nanoid'
+
+// HEAD DATA
+export const headData = {
+  title: 'ThElias',
+  lang: 'en',
+  description: 'ThElias personal website'
+}
+
+// HERO DATA
+export const heroData = {
+  title: 'Hey there, I am',
+  name: 'Toluwalase Elias Akande',
+  subtitle: 'This is where you get a glimpse of my professional life',
+  cta: 'Who Am I?'
+}
+
+// ABOUT DATA
+export const aboutData = {
+  img: 'profile.jpg',
+  paragraphOne: `
+    I am a Software developer with over three years of experience building software and creating software
+     solutions using mostly JavaScript. `,
+  paragraphTwo: `I major in the use of React for frontend development and NodeJs for 
+  backend development. I am good with databases like MongoDB, Postgresql, and Firestore.  I am good with
+   agile method of software development with good experience using Trello and pivotal tracker amongst
+    others.`,
+  paragraphThree: ` I make use of tools like version control with GitHub and continuous integration using 
+  Circle CI and GitHub actions more recently. I enjoy creating and learning about software and
+   always love a good challenge.`,
+  resume: 'https://drive.google.com/folderview?id=1ineTWoZuET578hGs-tqaqoayoboyKM_n'
+}
+
+// PROJECTS DATA
+export const projectsData = [
+  {
+    id: nanoid(),
+    img: 'project.jpg',
+    title: '',
+    info: '',
+    info2: '',
+    url: '',
+    repo: 'https://github.com/cobidev/react-simplefolio' // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'project.jpg',
+    title: '',
+    info: '',
+    info2: '',
+    url: '',
+    repo: 'https://github.com/cobidev/react-simplefolio' // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'project.jpg',
+    title: '',
+    info: '',
+    info2: '',
+    url: '',
+    repo: 'https://github.com/cobidev/react-simplefolio' // if no repo, the button will not show up
+  }
+]
+
+// CONTACT DATA
+export const contactData = {
+  cta: 'Wanna work with me or just have a chat? Great!',
+  btn: '',
+  email: 'akandetoluwalase@gmail.com'
+}
+
+// FOOTER DATA
+export const footerData = {
+  networks: [
+    {
+      id: nanoid(),
+      name: 'twitter',
+      url: ''
+    },
+    {
+      id: nanoid(),
+      name: 'codepen',
+      url: ''
+    },
+    {
+      id: nanoid(),
+      name: 'linkedin',
+      url: ''
+    },
+    {
+      id: nanoid(),
+      name: 'github',
+      url: ''
+    }
+  ]
+}
+
+// Github start/fork buttons
+export const githubButtons = {
+  isEnabled: true // set to false to disable the GitHub stars/fork buttons
+}
