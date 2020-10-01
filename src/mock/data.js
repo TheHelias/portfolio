a24012309497b823cbc00bd65dd213efd3e16bdd
TheHelias <akande.tolu@yahoo.com>
2020-10-01 21:13:38 +0100

    feat: improve about content

--- src/mock/data.js
+++ src/mock/data.js
@@ -19,15 +19,23 @@ export const heroData = {
 export const aboutData = {
   img: 'profile.jpg',
   paragraphOne: `
-    I am a Software developer with over three years of experience building software and creating software
-     solutions using mostly JavaScript. `,
-  paragraphTwo: `I major in the use of React for frontend development and NodeJs for 
-  backend development. I am good with databases like MongoDB, Postgresql, and Firestore.  I am good with
+    I am a Fullstack developer with over three years of quality experience building software and creating software
+     solutions. `,
+  paragraphTwo: `I consistently use React and some of it's frameworks like Next and Gatsby for frontend development and NodeJs with express for 
+  backend development(building APIs). I constantly use various databases like MongoDB, Postgresql, and Firestore.  I am good with
    agile method of software development with good experience using Trello and pivotal tracker amongst
     others.`,
-  paragraphThree: ` I make use of tools like version control with GitHub and continuous integration using 
-  Circle CI and GitHub actions more recently. I enjoy creating and learning about software and
+  paragraphThree: `I am also highly skilled in the use of tools like GitHub for version control and
+  Circle CI, GitHub actions for continuous integration. I have good knowledge and experience using Netlify and Heroku for deploying applications. Of course, I use 
+  many other tools asides the aforementioned which can't be contained here and I easily pick up new ones daily. I enjoy creating, learning and
    always love a good challenge.`,
+  paragraphFour: `In terms of work experience, it started in 2017 when I co-founded a peer-to-peer learning community where
+   I functioned as a hands-on tutor. From there, I went on to working various freelance and contract jobs with the notable one being
+   Servicemart - a startup where I worked as a Fullstack Developer in a team of 3 to build their service listing and hiring platform. Later on, I went on to work at EpowerNg - 
+   a Web and App agency where I functioned as the Frontend Developer for 9 months. I worked on 
+   many interesting projects there with the most notable ones being; building the frontend of a client's
+   accounting,  inventory & customer loyalty platform using React and building the company's current 
+   website using NextJs. I currently work freelance and I am very open to full time opportunities.`,
   resume: 'https://drive.google.com/folderview?id=1ineTWoZuET578hGs-tqaqoayoboyKM_n'
 }
 
