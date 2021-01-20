c521af6f75bf7804af7eee8d1819ed54e7723c39
TheHelias <akande.tolu@yahoo.com>
2021-01-20 15:47:39 +0100

    feat: add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,6 +20,12 @@ module.exports = {
       }
     },
     {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID || 'none'
+      }
+    },
+    {
       resolve: 'gatsby-plugin-manifest',
       options: {
         name: 'ThElias Portfolio',
