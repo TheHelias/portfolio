1f531dad38aabb7b42bb1d6a8f0a951983d2cfa1
TheHelias <akande.tolu@yahoo.com>
2021-01-20 13:41:39 +0100

    feat: add more portfolio data

--- src/mock/data.js
+++ src/mock/data.js
@@ -9,17 +9,17 @@ export const headData = {
 
 // HERO DATA
 export const heroData = {
-  title: 'Hey there, I am',
-  name: 'Toluwalase Elias Akande',
-  subtitle: 'This is where you get a glimpse of my professional life',
-  cta: 'Who Am I?'
+  title: 'Hi, I\'m',
+  name: 'Elias Akande',
+  subtitle: 'I am a Full-stack Engineer',
+  cta: 'Details?'
 }
 
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
   paragraphOne: `
-  I am a Full-stack engineer with over two years of quality experience building web applications. `,
+  I am a Full-stack engineer with 3 years of professional experience building web applications. `,
   paragraphTwo: `I consistently use React and some of its frameworks like Next and Gatsby for frontend
    development and NodeJs with express for backend development. I constantly use various databases like
     MongoDB, Postgresql, and Firestore. I am good with the agile method of software development with
@@ -29,12 +29,12 @@ export const aboutData = {
     deploying applications. Of course, I use many other tools asides from the aforementioned
      which can't be contained here and I easily pick up new ones daily. I enjoy
       creating, learning and always love a good challenge.`,
-  paragraphFour: `In terms of work experience, it started in 2017 when I co-founded 
-  a peer-to-peer learning community where I functioned as a hands-on tutor. From there, 
+  paragraphFour: `In 2017, I co-founded 
+  a peer-to-peer learning community for developers where I functioned as a hands-on tutor. From there, 
   I went on to working various freelance and contract jobs with the notable one being Servicemart 
-  - a startup where I worked as a Fullstack Developer in a team of 3 to build their service
+  - a startup where I worked as a Fullstack Engineer in a team of 3 to build their service
    listing and hiring platform. Later on, I went on to work at EpowerNg - a Web and App agency
-    where I functioned as the Frontend Developer for 9 months. I worked on many challenging and
+    where I functioned as the Frontend Engineer for 9 months. I worked on many challenging and
      interesting projects there with the company’s clients. I currently work freelance and 
      I am very open to full-time opportunities.`,
   resume: 'https://drive.google.com/folderview?id=1ineTWoZuET578hGs-tqaqoayoboyKM_n'
@@ -46,15 +46,15 @@ export const projectsData = [
     id: nanoid(),
     img: 'project_tp.png',
     title: 'Modern Brand Website',
-    info: 'A modern brand website built using React and Chakra UI',
+    info: 'A modern brand website for a private security company built using React and Chakra UI',
     info2: '',
-    url: 'https://sleepy-fermi-4f2686.netlify.app/'
+    url: 'https://topprivatesecurity.com/'
   },
   {
     id: nanoid(),
     img: 'project_ma.png',
     title: 'Filmmaking Portfolio Website and Blog',
-    info: 'A filmmaking portfolio website and blog built using React, Gatsby, Bulma etc',
+    info: 'A filmmaking portfolio website and blog built using Gatsby, Netlify CMS, Bulma, SASS etc',
     info2: 'Check out the source code for more info',
     url: 'https://martiniakande.com/',
     repo: 'https://github.com/TheHelias/signor-martini-portfolio'
@@ -63,16 +63,24 @@ export const projectsData = [
     id: nanoid(),
     img: 'project_pc.png',
     title: 'Promise Cup Website',
-    info: 'A brand website built using Wordpress, CSS and jQuery',
+    info: 'A brand website for a company focused on improving the health of women',
     info2: '',
     url: 'https://promisecup.com/'
   },
   {
     id: nanoid(),
+    img: 'project_sm.png',
+    title: 'A service listing and hiring platform',
+    info: 'Built using NodeJs(Express) and Postgresql ',
+    info2: '',
+    repo: 'https://github.com/TheHelias/service_mart_api'
+  },
+  {
+    id: nanoid(),
     img: 'project_tc.png',
     title: 'An event registering platform',
-    info: 'A simple CRUD app demonstrating CRUD.',
-    info2: 'Built using NodeJs(Express) and React',
+    info: 'A simple app for an event listing platform. ',
+    info2: 'Built using NodeJs(Express), MongoDB and React',
     url: 'https://talk-conference-app.herokuapp.com/',
     repo: 'https://github.com/TheHelias/conference-talk-app'
   }
@@ -81,8 +89,8 @@ export const projectsData = [
 // CONTACT DATA
 export const contactData = {
   cta: 'Wanna work with me or just have a chat? Great!',
-  btn: '',
-  email: 'akandetoluwalase@gmail.com'
+  btn: 'Let\'s Talk',
+  link: 'https://wa.me/+2348166746401'
 }
 
 // FOOTER DATA
@@ -111,7 +119,7 @@ export const footerData = {
     {
       id: nanoid(),
       name: 'whatsapp',
-      url: 'https://wa.link/mossy5'
+      url: 'https://wa.me/+2348166746401'
     },
     {
       id: nanoid(),
