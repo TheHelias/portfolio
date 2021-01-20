450f36776b266b967be625486c40c3644a85cbb7
TheHelias <akande.tolu@yahoo.com>
2021-01-20 11:29:35 +0100

    feat: edit about data

--- src/mock/data.js
+++ src/mock/data.js
@@ -19,23 +19,24 @@ export const heroData = {
 export const aboutData = {
   img: 'profile.jpg',
   paragraphOne: `
-    I am a Fullstack developer with over three years of quality experience building software and creating software
-     solutions. `,
-  paragraphTwo: `I consistently use React and some of it's frameworks like Next and Gatsby for frontend development and NodeJs with express for 
-  backend development(building APIs). I constantly use various databases like MongoDB, Postgresql, and Firestore.  I am good with
-   agile method of software development with good experience using Trello and pivotal tracker amongst
-    others.`,
-  paragraphThree: `I am also highly skilled in the use of tools like GitHub for version control and
-  Circle CI, GitHub actions for continuous integration. I have good knowledge and experience using Netlify and Heroku for deploying applications. Of course, I use 
-  many other tools asides the aforementioned which can't be contained here and I easily pick up new ones daily. I enjoy creating, learning and
-   always love a good challenge.`,
-  paragraphFour: `In terms of work experience, it started in 2017 when I co-founded a peer-to-peer learning community where
-   I functioned as a hands-on tutor. From there, I went on to working various freelance and contract jobs with the notable one being
-   Servicemart - a startup where I worked as a Fullstack Developer in a team of 3 to build their service listing and hiring platform. Later on, I went on to work at EpowerNg - 
-   a Web and App agency where I functioned as the Frontend Developer for 9 months. I worked on 
-   many interesting projects there with the most notable ones being; building the frontend of a client's
-   accounting,  inventory & customer loyalty platform using React and building the company's current 
-   website using NextJs. I currently work freelance and I am very open to full time opportunities.`,
+  I am a Full-stack engineer with over two years of quality experience building web applications. `,
+  paragraphTwo: `I consistently use React and some of its frameworks like Next and Gatsby for frontend
+   development and NodeJs with express for backend development. I constantly use various databases like
+    MongoDB, Postgresql, and Firestore. I am good with the agile method of software development with
+     good experience using Trello and pivotal tracker amongst others.`,
+  paragraphThree: `I am also a big fan of testing and collaborating using version control tools.
+   I have good knowledge and experience using Netlify, Vercel, and Heroku amongst others for
+    deploying applications. Of course, I use many other tools asides from the aforementioned
+     which can't be contained here and I easily pick up new ones daily. I enjoy
+      creating, learning and always love a good challenge.`,
+  paragraphFour: `In terms of work experience, it started in 2017 when I co-founded 
+  a peer-to-peer learning community where I functioned as a hands-on tutor. From there, 
+  I went on to working various freelance and contract jobs with the notable one being Servicemart 
+  - a startup where I worked as a Fullstack Developer in a team of 3 to build their service
+   listing and hiring platform. Later on, I went on to work at EpowerNg - a Web and App agency
+    where I functioned as the Frontend Developer for 9 months. I worked on many challenging and
+     interesting projects there with the company’s clients. I currently work freelance and 
+     I am very open to full-time opportunities.`,
   resume: 'https://drive.google.com/folderview?id=1ineTWoZuET578hGs-tqaqoayoboyKM_n'
 }
 
@@ -56,7 +57,7 @@ export const projectsData = [
     info: 'A filmmaking portfolio website and blog built using React, Gatsby, Bulma etc',
     info2: 'Check out the source code for more info',
     url: 'https://martiniakande.com/',
-    repo: 'https://github.com/TheHelias/signor-martini-portfolio' // if no repo, the button will not show up
+    repo: 'https://github.com/TheHelias/signor-martini-portfolio'
   },
   {
     id: nanoid(),
@@ -73,7 +74,7 @@ export const projectsData = [
     info: 'A simple CRUD app demonstrating CRUD.',
     info2: 'Built using NodeJs(Express) and React',
     url: 'https://talk-conference-app.herokuapp.com/',
-    repo: 'https://github.com/TheHelias/conference-talk-app' // if no repo, the button will not show up
+    repo: 'https://github.com/TheHelias/conference-talk-app'
   }
 ]
 
@@ -120,7 +121,6 @@ export const footerData = {
   ]
 }
 
-// Github start/fork buttons
 export const githubButtons = {
-  isEnabled: false // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false
 }
